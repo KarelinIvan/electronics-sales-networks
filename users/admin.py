fe0755e01af6786a05972ab8b1bfd0192bb56832
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-12 18:56:18 +0300

    17-й коммит

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from users.models import User
+
 # Register your models here.
+
+
+@admin.register(User):
+class UserAdmin(admin.ModelAdmin):
+    """ Панель администратора """
+    list_display = ('username', 'email', 'is_staff', 'created_at')
+    search_fields = ['username', 'email']
+    list_filter = ['is_staff', 'created_at']
--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """ Пользователи сети """
+    username = models.CharField(max_length=25, verbose_name='Имя пользователя')
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(max_length=20, verbose_name='Телефон', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='Город', **NULLABLE)
+    company = models.CharField(
+        max_length=255, verbose_name='Компания', **NULLABLE)
+    position = models.CharField(
+        max_length=100, verbose_name='Должность', **NULLABLE)
+    level = models.PositiveSmallIntegerField(
+        choices=((0, 'Завод'), (1, 'Розничная сеть'),
+                 (2, 'Индивидуальный предприниматель')),
+        verbose_name='Уровень доступа',
+        default=0
+    )
+    is_active = models.BooleanField(default=True, verbose_name='Активен')
+    is_staff = models.BooleanField(
+        default=False, verbose_name='Статус сотрудника')
+    date_joined = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата регистрации')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
