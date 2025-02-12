943713e5ed5c5cb7421ebb56a1ac05e30e3696ff
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-02-12 15:01:00 +0300

    13-й коммит

--- network/urls.py
+++ network/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from network.views import NetworkElementViewSet, ProductViewSet
 
 router = DefaultRouter()
-router.register(r'elements', NetworkElementViewSet, name='element')
-router.register(r'products', ProductViewSet, name='product')
+router.register(r'elements', NetworkElementViewSet, basename='element')
+router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('', include(router.urls)),
